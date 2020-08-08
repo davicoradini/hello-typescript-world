293f95a863c67fba902ce1fc9b018e25fb3890ae
Davi Siepmann <davicoradini@gmail.com>
2020-08-08 11:10:21 -0400

    add ts stuff

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
 var world = '🗺️';
 var appDiv = document.getElementById('app');
+var var1 = { user: { id: 1, name: "User" } };
+var userId = var1 === null || var1 === void 0 ? void 0 : var1.user.id;
+console.log("User id using Typescript 3.8 optional chaining: " + userId);
 if (appDiv) {
     appDiv.innerHTML = "<h1>Hello " + world + "</h1>";
-    console.log("hello " + world);
 }
--- index.ts
+++ index.ts
@@ -2,7 +2,10 @@ const world = '🗺️';
 
 const appDiv: HTMLElement | null = document.getElementById('app');
 
+const var1 = { user: { id: 1, name: "User" } }
+const userId = var1?.user.id;
+console.log(`User id using Typescript 3.8 optional chaining: ${userId}`);
+
 if (appDiv) {
     appDiv.innerHTML = `<h1>Hello ${world}</h1>`;
-    console.log(`hello ${world}`);
 }
